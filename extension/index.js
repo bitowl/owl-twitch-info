bc9c9b62f4c5fca5f81d3c5cc9ee99f4e330d9e4
bitowl <git@bitowl.de>
2019-09-06 00:37:33 +0200

    Adapt to new Twitch api

--- extension/index.js
+++ extension/index.js
@@ -1,64 +1,104 @@
-'use strict';
+"use strict";
 
-module.exports = function (nodecg) {
-    const twitchApi = nodecg.extensions['lfg-twitchapi'];
+module.exports = function(nodecg) {
+  const twitchApi = nodecg.extensions["lfg-twitchapi"];
 
-    if (twitchApi === undefined) {
-        nodecg.log.error('The lfg-twitchapi needs to be installed and configured for this bundle to work');
-        return;
+  if (twitchApi === undefined) {
+    nodecg.log.error(
+      "The lfg-twitchapi needs to be installed and configured for this bundle to work"
+    );
+    return;
+  }
+
+  const loadedRepl = nodecg.Replicant("dashboard-loaded", {
+    defaultValue: false,
+    persistent: false
+  });
+  const statusRepl = nodecg.Replicant("status", {
+    defaultValue: "Refresh page to load info...",
+    persistent: false
+  });
+  const gameRepl = nodecg.Replicant("game", {
+    defaultValue: "",
+    persistent: false
+  });
+  const liveRepl = nodecg.Replicant("live", {
+    defaultValue: false,
+    persistent: false
+  });
+  const streamStartRepl = nodecg.Replicant("start", {
+    defaultValue: null,
+    persistent: false
+  });
+  const viewersRepl = nodecg.Replicant("viewers", {
+    defaultValue: 0,
+    persistent: false
+  });
+  const viewsRepl = nodecg.Replicant("views", {
+    defaultValue: 0,
+    persistent: false
+  });
+  const followersRepl = nodecg.Replicant("followers", {
+    defaultValue: 0,
+    persistent: false
+  });
+
+  var requesting = false;
+
+  loadedRepl.on("change", newVal => {
+    if (newVal && !requesting) {
+      requesting = true;
+      setInterval(getInfo, nodecg.bundleConfig.updateInterval * 1000);
+      getInfo();
     }
+  });
 
-    const loadedRepl = nodecg.Replicant('dashboard-loaded', {defaultValue: false, persistent: false});
-    const statusRepl = nodecg.Replicant('status', {defaultValue: 'Refresh page to load info...', persistent: false});
-    const gameRepl = nodecg.Replicant('game', {defaultValue: '', persistent: false});
-    const liveRepl = nodecg.Replicant('live', {defaultValue: false, persistent: false});
-    const streamStartRepl = nodecg.Replicant('start', {defaultValue: null, persistent: false});
-    const viewersRepl = nodecg.Replicant('viewers', {defaultValue: 0, persistent: false});
-    const viewsRepl = nodecg.Replicant('views', {defaultValue: 0, persistent: false});
-    const followersRepl = nodecg.Replicant('followers', {defaultValue: 0, persistent: false});
-
-    var requesting = false;
-
-    loadedRepl.on('change', newVal => {
-        if (newVal && !requesting) {
-            requesting = true;
-            setInterval(getInfo, nodecg.bundleConfig.updateInterval * 1000);
-            getInfo();
+  function getInfo() {
+    twitchApi
+      .get("/streams?user_id=" + nodecg.bundleConfig.channelId, {})
+      .then(response => {
+        if (response.statusCode !== 200) {
+          nodecg.log.error(response.body.error, response.body.message);
+          return;
         }
-    });
 
-    
+        if (response.body.data.length === 0) {
+          liveRepl.value = false;
+          return;
+        }
+        var stream = response.body.data[0];
 
-    function getInfo() {
-        twitchApi.get('/streams/{{username}}', {}).then(response => {
-            if (response.statusCode !== 200) {
-                nodecg.log.error(response.body.error, response.body.message);
-                return;
-            }
-            var stream = response.body.stream;
+        liveRepl.value = stream.viewer_count;
+        streamStartRepl.value = stream.started_at;
+        statusRepl.value = stream.title;
+      })
+      .catch(err => {
+        nodecg.log.error("Api error", err);
+      });
+    twitchApi
+      .get("/users?id=" + nodecg.bundleConfig.channelId, {})
+      .then(response => {
+        if (response.statusCode !== 200) {
+          nodecg.log.error(response.body.error, response.body.message);
+          return;
+        }
+        viewsRepl.value = response.body.data[0].view_count;
+      })
+      .catch(err => {
+        nodecg.log.error("Api error", err);
+      });
 
-            if (stream == null) {
-                liveRepl.value = false;
-                return;
-            }
-
-            liveRepl.value = stream.viewers;
-            streamStartRepl.value = stream.created_at;
-        }).catch(err => {
-            nodecg.log.error('Api error', err);
-        });
-        twitchApi.get('/channels/{{username}}', {}).then(response => {
-            if (response.statusCode !== 200) {
-                nodecg.log.error(response.body.error, response.body.message);
-                return;
-            }
-
-            statusRepl.value = response.body.status;
-            viewsRepl.value = response.body.views;
-            followersRepl.value = response.body.followers;
-
-        }).catch(err => {
-            nodecg.log.error('Api error', err);
-        });
-    }
+    twitchApi
+      .get("/users/follows?to_id=" + nodecg.bundleConfig.channelId, {})
+      .then(response => {
+        if (response.statusCode !== 200) {
+          nodecg.log.error(response.body.error, response.body.message);
+          return;
+        }
+        followersRepl.value = response.body.total;
+      })
+      .catch(err => {
+        nodecg.log.error("Api error", err);
+      });
+  }
 };
